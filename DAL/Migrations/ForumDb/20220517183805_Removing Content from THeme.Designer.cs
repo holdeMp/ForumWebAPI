b33b308c6bc8c64a3fcd094e0c8211abb7e24594
Marii Bohdan <molotok.marii@gmail.com>
2022-05-17 22:02:54 +0300

    Added migrations

--- DAL/Migrations/ForumDb/20220517183805_Removing Content from THeme.Designer.cs
+++ DAL/Migrations/ForumDb/20220517183805_Removing Content from THeme.Designer.cs
@@ -4,14 +4,16 @@ using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Data.Migrations.ForumDb
 {
     [DbContext(typeof(ForumDbContext))]
-    partial class ForumDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220517183805_Removing Content from THeme")]
+    partial class RemovingContentfromTHeme
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DAL/Migrations/ForumDb/20220517183805_Removing Content from THeme.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations.ForumDb
+{
+    public partial class RemovingContentfromTHeme : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Themes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Themes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
